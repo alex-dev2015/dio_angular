fd5af985f28aba512269a3e12fbae6405234b245
AlexSousa <alex.sousa20@hotmail.com>
2020-12-29 17:38:03 -0300

    Implementação do filtro de busca

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -7,14 +7,29 @@ import { CourseService } from './course.service';
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
-    courses: Course[] = [];
+    filteredCourses: Course[] = [];
+    
+    _courses: Course[] = [];
+
+    _filterBy: string;
 
     constructor(private courseService: CourseService){
 
     }
 
-ngOnInit():void{
-    this.courses = this.courseService.retrieveAll();
-}
+    ngOnInit():void{
+        this._courses = this.courseService.retrieveAll();
+        this.filteredCourses = this._courses;
+    }
+
+    set filter(value: string){
+        this._filterBy = value;
+
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1)
+    }
+
+    get filter(){
+        return this._filterBy;
+    }
 
 }
